bef120991546ec43f78bab970fa52b87c3ac1f10
lunarrrres <vinyarsa@gmail.com>
2026-05-21 12:25:16 +0300

    feat: integrate PostHog for analytics tracking

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import posthog from "posthog-js";
+posthog.init("<ph_project_api_key>", {
+  api_host: "<ph_instance_address>",
+  person_profiles: "identified_only", // або 'always' для анонімних користув
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
